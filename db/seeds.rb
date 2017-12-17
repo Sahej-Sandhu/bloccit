82ef53e10642fe643e67b55fe07eaa90edfda477
Sahejpreet Sandhu <indango@gmail.com>
2017-12-16 22:38:35 -0800

    Assignment 20 rails topics and posts: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,14 @@ require 'random_data'
 end
 topics = Topic.all
 
+5.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+
  50.times do
    Post.create!(
      topic: topics.sample,
@@ -27,4 +35,5 @@ topics = Topic.all
  puts "Seed finished"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
+ puts "#{SponsoredPost.count} sponsored posts created"
  puts "#{Comment.count} comments created"
