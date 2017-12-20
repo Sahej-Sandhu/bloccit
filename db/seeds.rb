e96819e6abf7f989681a80bb1b4b4d68bdbce047
Sahejpreet Sandhu <indango@gmail.com>
2017-12-20 11:23:44 -0800

    Checkpoint 27 rails authorization: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,11 +34,18 @@ topics = Topic.all
    )
  end
 
-user = User.first
-user.update_attributes!(
-  email: 'indango@gmail.com',
-  password: 'helloworld'
-)
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
 
  puts "Seed finished"
  puts "#{User.count} users created"
